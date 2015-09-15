25c7f5da965987761cb7946d7afe74c2b034adc3
Deepak Bhaskaran <dbhaskaran@pindropsecurity.com>
2015-09-15 09:02:22 -0400

    Added in place holder pages - about, blog and contact. Fixex about.

--- hello.py
+++ hello.py
@@ -15,8 +15,14 @@ def page_not_found(e):
 
 
 @app.route("/")
+@app.route("/index.html")
 def index():
     return render_template('index.html')
+
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
