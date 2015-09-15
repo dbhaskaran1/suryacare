00fa74d523c9ed00d2b500298869d16c9703a86c
Deepak Bhaskaran <dbhaskaran@pindropsecurity.com>
2015-09-15 08:35:06 -0400

    Bootstrapped UI

--- hello.py
+++ hello.py
@@ -1,11 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static", static_folder="static")
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 @app.route("/")
-def hello():
-    return "Hello world!"
+def index():
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
